79ffea543824c3350500d65d07b5d12ef4d0ca9c
selinamelia <sellinaamelia@gmail.com>
2021-08-24 17:56:34 +0700

    created parser file

---
+++ parsers/listings.rb
@@ -0,0 +1,13 @@
+nokogiri = Nokogiri.HTML(content)
+
+products = nokogiri.css('#searchProductResult li')
+products.each do |product|
+  href = product.at_css('a.product-title-link')['href']
+  url = URI.join('https://www.walmart.com', href).to_s
+  pages << {
+      url: url,
+      page_type: 'products',
+      fetch_type: 'browser',
+      vars: {}
+    }
+end
