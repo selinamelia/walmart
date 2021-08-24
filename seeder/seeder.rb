d20aec7411051a80cc90beb60ec9f58d3ed7eee8
selinamelia <sellinaamelia@gmail.com>
2021-08-24 23:26:55 +0700

    add driver code b

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,26 +1,26 @@
-# click_captha_code = "
-#     await sleep(3000);
-#     if ( (await page.$('div#js-global-footer-wrapper form#hf-email-signup-form')) == null){
-#         await sleep(5412);
-#         if ( (await page.$('iframe[style*=\"display: block\"]')) !== null) {
-#             await page.hover('iframe[style*=\"display: block\"]'); await sleep(1428);
+click_captha_code = "
+    await sleep(3000);
+    if ( (await page.$('div#js-global-footer-wrapper form#hf-email-signup-form')) == null){
+        await sleep(5412);
+        if ( (await page.$('iframe[style*=\"display: block\"]')) !== null) {
+            await page.hover('iframe[style*=\"display: block\"]'); await sleep(1428);
 
-#             await Promise.all([
-#                 page.waitForNavigation(),
-#                 page.click('iframe[style*=\"display: block\"]', {delay: 9547}),
-#             ]);          
-#         };
-#     };
-# "
+            await Promise.all([
+                page.waitForNavigation(),
+                page.click('iframe[style*=\"display: block\"]', {delay: 9547}),
+            ]);          
+        };
+    };
+"
 
 pages << {
-    url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+    url: "https://www.walmart.com/browse/movies-tv-shows/tv-shows/4096_530719?cat_id=4096_530719&facet=new_releases%3ALast+90+Days",
     page_type: "listings",
     method: "GET",
     fetch_type: 'browser',
     force_fetch: true,
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
     driver: {
-        code: page.click('iframe[style*=\"display: block\"]', {delay: 9547})
-      }
+        code: click_captha_code
+    }
 }
