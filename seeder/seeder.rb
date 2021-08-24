37cc78730157c7aea36a6ce582fac3bdb3b1ab43
selinamelia <sellinaamelia@gmail.com>
2021-08-24 17:43:13 +0700

    created a seeder file

---
+++ seeder/seeder.rb
@@ -0,0 +1,8 @@
+pages << {
+    url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+    page_type: "listings",
+    method: "GET",
+    fetch_type: 'browser',
+    force_fetch: true,
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+}
