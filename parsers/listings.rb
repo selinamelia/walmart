c9ad0e0e5501e756528b916798e3c498e2f7fc2e
selinamelia <sellinaamelia@gmail.com>
2021-08-24 18:18:25 +0700

    fix listings rb

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,13 +1,15 @@
 nokogiri = Nokogiri.HTML(content)
 
-products = nokogiri.css('#searchProductResult li')
+products = nokogiri.css('div.search-result-product-title gridview')
 products.each do |product|
-    href = product.at_css('a.product-title-link')['href']
-    url = URI.join('https://www.walmart.com', href).to_s
-    pages << {
-        url: url,
-        page_type: 'products',
-        fetch_type: 'browser',
-        vars: {}
-    }
+    href = product.at_css('a.product-title-link')
+    if href
+        url = URI.join('https://www.walmart.com', href['href'])
+        pages << {
+            url: url,
+            page_type: 'products',
+            fetch_type: 'browser',
+            #vars: {}
+        }
+    end
 end
