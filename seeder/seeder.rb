18c4797275775218cfd9267f9e85e3758a748f17
selinamelia <sellinaamelia@gmail.com>
2021-08-25 23:02:35 +0700

    change url in seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -23,7 +23,6 @@ pages << {
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   driver: {
-    enable_images: true,
     code: click_captha_code
   }
 }
