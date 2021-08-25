e09a1c37d790b4190a6e32ad6f11f11e53998002
selinamelia <sellinaamelia@gmail.com>
2021-08-25 17:27:21 +0700

    comment driver code

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -23,7 +23,7 @@ pages << {
   force_fetch: true,
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  driver: {
-    code: click_captha_code
-  }
+  # driver: {
+  #   code: click_captha_code
+  # }
 }
