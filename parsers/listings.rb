102cf20ba23e3fb4371c0e68831d821e90978031
selinamelia <sellinaamelia@gmail.com>
2021-08-25 17:46:36 +0700

    fix listings

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,4 +1,21 @@
 nokogiri = Nokogiri.HTML(content)
+click_captha_code = " 
+  await sleep(3000);
+  if ( (await page.$('div#js-global-footer-wrapper form#hf-email-signup-form')) == null){
+    await sleep(5412);
+    if ( (await page.$('iframe[style*=\"display: block\"]')) !== null) {
+      // do things with its content
+      await page.hover('iframe[style*=\"display: block\"]'); await sleep(1428); 
+      // click hold and wait loading new page
+      await Promise.all([
+        page.waitForNavigation(),
+        page.click('iframe[style*=\"display: block\"]', {delay: 9547}),
+      ]);
+    };
+  };
+  await page.setPageLimitLoadingTime(3000);
+"
+
 
 products = nokogiri.css('li.Grid-col')
 products.each do |product|
@@ -12,6 +29,9 @@ products.each do |product|
             method: "GET",
             force_fetch: true
             #vars: {}
+            driver: {
+                code: click_captha_code
+            }
         }
     end
 end
