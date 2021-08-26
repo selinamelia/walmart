92f315968c6615910c1bacde0dd5390be04d78f6
selinamelia <sellinaamelia@gmail.com>
2021-08-26 20:29:03 +0700

    fix driver code

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -38,3 +38,14 @@ products.each do |i|
         }
     end
 end
+
+# pagination_links = nokogiri.css('ul.paginator-list li a')
+# pagination_links.each do |link|
+#   url = URI.join('https://www.walmart.com', link['href']).to_s
+#   pages << {
+#       url: url,
+#       page_type: 'listings',
+#       fetch_type: 'browser',
+#       vars: {}
+#     }
+# end
